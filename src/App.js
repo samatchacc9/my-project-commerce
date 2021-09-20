5701c38e9f660acf900b2955aac68c529551f8ba
samatchacc9 <samatcha.matcha@gmail.com>
2021-09-20 21:47:56 +0700

    navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import './App.css';
 
 function App() {
-  return <h1>Hello</h1>;
+  return (
+    <nav>
+      <div className='brand'>
+        Life<font>Great</font>
+      </div>
+
+      <ul className='menu-list'>
+        <li>
+          <a href='#' className='btn bg-info text-white'>
+            <i className='fas fa-user-plus'> Login</i>
+          </a>
+        </li>
+        <li>
+          <a href='#' className='btn bg-secondary text-white'>
+            <i className='fas fa-sign-in-alt'> Register</i>
+          </a>
+        </li>
+      </ul>
+      <div className='menu-icon'>
+        <i className='fas fa-bars'></i>
+      </div>
+    </nav>
+  );
 }
 
 export default App;
